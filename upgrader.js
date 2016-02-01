6796995159ebda5753cfc0b8f838206d8c24f043
akuzmenok <akuzmenok@linguahouse.com>
2016-02-01 12:39:23 +0100

    LIN-364 initial commit of npm package files

---
+++ upgrader.js
@@ -0,0 +1,48 @@
+require('./libs/upgrader-lib.js');
+var ArgumentParser = require('argparse').ArgumentParser;
+var _ = require('underscore');
+
+var parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Mongo Migration script'
+});
+
+parser.addArgument(
+    [ '-u', '--host', '--url' ],
+    {
+        help: 'MongoDB host address',
+        required: true
+    }
+);
+parser.addArgument(
+    [ '-d', '--database' ],
+    {
+        help: 'MongoDB database name',
+        required: true
+    }
+);
+parser.addArgument(
+    [ '-f', '--folder' ],
+    {
+        help: 'MongoDB migration scripts path',
+        required: true
+    }
+);
+
+var args_dirty = parser.parseArgs();
+var args = {};
+_.mapObject(args_dirty, function(item, key) {
+    if (item !== null) {
+        args[key] = item;
+    }
+});
+
+var options = {
+    host: 'localhost',
+    db: 'app'
+};
+
+options = _.extend(options, args);
+
+runMigrationFuture(options);
